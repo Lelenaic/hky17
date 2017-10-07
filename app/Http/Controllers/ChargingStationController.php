e082034e113c3559050ed660882b9d7a6c71463d
Flavien Chêne <me@flavienchene.fr>
2017-10-07 14:33:11 +0200

    Ajout charging_station

---
+++ app/Http/Controllers/ChargingStationController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ChargingStation;
+use Illuminate\Http\Request;
+
+class ChargingStationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $c = new ChargingStation();
+        return $c->all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $r
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $r)
+    {
+        $r->validate([
+            'name' => 'required',
+        ]);
+        $c = new ChargingStation();
+        $c->name = $r->name;
+
+        $c->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $r
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $r, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
